e9e14b5ad8dd3bdfc6055e769bfd3a2da306ed08
anranzhen <1791287286@qq.com>
2019-11-09 23:02:18 +0800

    新增：待办事项列表显示

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -6,8 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'tasklist.html'
 })
 export class TaskListPage {
+  tasks:Array<any> = [];
 
   constructor(public navCtrl: NavController) {
+
+  }
+  ionViewDidLoad(){
+    this.tasks=[
+      {title:'牛奶',status:'open'},
+      {title:'鸡蛋',status:'open'},
+      {title:'果汁',status:'open'},
+      {title:'煎饼',status:'open'}
+    ];
+  }
+
+  addTask(){
 
   }
 
